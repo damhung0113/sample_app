6ae7425a0c3b92f45d2c1b8d3f4d4b108e7e2ecc
damhung0113 <tienhuzng@gmail.com>
2019-06-26 17:49:45 +0700

    Chapter 6: Modeling Users

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
 require_relative "boot"
-
 require "rails/all"
-
-Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    Bundler.require *Rails.groups
+    Config::Integrations::Rails::Railtie.preload
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
