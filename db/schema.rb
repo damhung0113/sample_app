f017d149a95abf37601d58b57657d0e9e941e79b
HungDam <bestdothua@gmail.com>
2019-07-05 11:42:45 +0700

    Chap 9: Advanced Login

--- db/schema.rb
+++ db/schema.rb
@@ -4,6 +4,7 @@ ActiveRecord::Schema.define(version: 2019_06_28_061540) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
