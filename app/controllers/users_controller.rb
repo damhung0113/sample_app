4bd2abc05544f03fa653f91903442779a98fa375
HungDam <bestdothua@gmail.com>
2019-07-07 00:47:05 +0700

    Chap 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,48 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(update edit index)
+  before_action :correct_user, only: %i(update edit)
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
+  def show; end
+
+  def index
+    @users = User.order(:name).page(params[:page]).per Settings.index.per_page
   end
 
   def create
     @user = User.new user_params
 
     if @user.save
-      flash[:success] = t "success"
+      flash[:success] = t ".success"
       redirect_to login_path param: @user.email
     else
       render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".error"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".delete_done"
+      redirect_to @user
+    else
+      flash[:danger] = t ".delete_failed"
     end
   end
 
@@ -23,5 +51,24 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t ".login_first"
+    redirect_to login_path
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.is_admin?
+  end
+
+  def correct_user
+    find_user
+    redirect_to edit_user_path current_user unless current_user? @user
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Dam Tien Hung",
+             email: "tienhuzng@gmail.com",
+             password: "123123",
+             password_confirmation: "123123",
+             is_admin: true)
+
+10.times do
+  User.create([{
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  }])
+end
